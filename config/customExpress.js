f0c3241b2f2c5faad37abc5b94933971b5f95cb1
bwendel26 <bwendel26999@gmail.com>
2021-06-03 18:01:35 -0300

    Adicionando o parser do express para urlencoded e JSON e testando nossa API no postman com POST e GET methods

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const consign = require('consign');
+const { urlencoded } = require('express');
 
 module.exports = () => {
     const app = express();
 
+    app.use(urlencoded({extended: true}));
+    app.use(express.json());
+    
     consign().include('controllers').into(app);
 
     return app;
